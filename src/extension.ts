03434b5456922dbca6308106112103c27303e57d
zhennann <zhen.nann@icloud.com>
2024-09-01 16:32:57 +0800

    feat: activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { logger } from './utils/outputChannel.js';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activateExtension(context: vscode.ExtensionContext) {
-  console.log(vscode.workspace.workspaceFolders?.[0].uri.path);
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "zova-vscode" is now active!');
+  logger.log('Zova-vscode is active');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'zova-vscode.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
+  context.subscriptions.push(
+    vscode.commands.registerCommand('zova-vscode.helloWorld', () => {
       vscode.window.showInformationMessage('Hello World from zova-vscode!');
-    }
+    })
   );
-
-  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivateExtension() {
-  console.log('deactivate');
-}
+export function deactivateExtension() {}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './global.js';
+export * from './outputChannel.js';
 export * from './zova.js';
