037c0cb5790c1a0f28da3c2cce82f5bcd0b52179
zhennann <zhen.nann@icloud.com>
2024-09-03 11:13:06 +0800

    feat: fileWatchers

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import { logger } from './utils/outputChannel.js';
+import { FileWatchers } from './watchers/files.js';
 
+let fileWatchers: FileWatchers;
 export function activateExtension(context: vscode.ExtensionContext) {
   logger.log('Zova-vscode is active');
 
@@ -9,6 +11,14 @@ export function activateExtension(context: vscode.ExtensionContext) {
       vscode.window.showInformationMessage('Hello World from zova-vscode!');
     })
   );
+
+  fileWatchers = new FileWatchers(context);
+  fileWatchers.start();
 }
 
-export function deactivateExtension() {}
+export function deactivateExtension() {
+  if (fileWatchers) {
+    fileWatchers.stop();
+    fileWatchers = undefined!;
+  }
+}
---
+++ src/watchers/files.ts
@@ -0,0 +1,33 @@
+import { ExtensionContext, FileSystemWatcher, workspace } from 'vscode';
+import { getProjectRootDirectory } from '../utils/global.js';
+
+export class FileWatchers {
+  context: ExtensionContext;
+  rootWatcher: FileSystemWatcher = undefined!;
+
+  constructor(context: ExtensionContext) {
+    this.context = context;
+  }
+
+  start() {
+    this.rootWatcher = workspace.createFileSystemWatcher(
+      getProjectRootDirectory()
+    );
+    this.rootWatcher.onDidCreate((uri) => {
+      console.log('create:', uri);
+    });
+    this.rootWatcher.onDidChange((uri) => {
+      console.log('change:', uri);
+    });
+    this.rootWatcher.onDidDelete((uri) => {
+      console.log('delete:', uri);
+    });
+  }
+
+  stop() {
+    if (this.rootWatcher) {
+      this.rootWatcher.dispose();
+      this.rootWatcher = undefined!;
+    }
+  }
+}
