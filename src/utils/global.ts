2437aecab201c3d8763f0cbfda852d544f4f612b
zhennann <zhen.nann@icloud.com>
2024-09-03 11:00:03 +0800

    Update global.ts

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -3,3 +3,25 @@ import * as vscode from 'vscode';
 export function getProjectRootDirectory(): string {
   return vscode.workspace.workspaceFolders?.[0].uri.fsPath!;
 }
+
+export function newTerminal(
+  terminalName: string,
+  command: string,
+  cwd?: string
+) {
+  const existingTerminal = vscode.window.terminals.find(
+    (terminal) => terminal.name === terminalName
+  );
+
+  if (existingTerminal) {
+    existingTerminal.show();
+    existingTerminal.sendText(command);
+  } else {
+    const terminal = vscode.window.createTerminal({
+      name: terminalName,
+      cwd,
+    });
+    terminal.sendText(command);
+    terminal.show();
+  }
+}
