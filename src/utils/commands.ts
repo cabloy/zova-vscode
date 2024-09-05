e14e222edafffc3608c9b25c14dfd3c4c133e759
zhennann <zhen.nann@icloud.com>
2024-09-05 16:09:21 +0800

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,5 +1,6 @@
 import { commands, ExtensionContext } from 'vscode';
 import { createLocalBean } from '../commands/bean.js';
+import { logger } from './outputChannel.js';
 
 const extensionCommands = [
   { command: 'zova.createLocalBean', function: createLocalBean },
@@ -15,8 +16,21 @@ export class Commands {
   initialize() {
     for (const { command, function: commandFunction } of extensionCommands) {
       this.context.subscriptions.push(
-        commands.registerCommand(command, commandFunction)
+        commands.registerCommand(
+          command,
+          wrapperCommand(command, commandFunction)
+        )
       );
     }
   }
+}
+
+function wrapperCommand(command, fn) {
+  return async function (...args) {
+    try {
+      await fn(...args);
+    } catch (err) {
+      logger.log(`command: ${command} Error: ${err.message}`);
+    }
+  };
 }
