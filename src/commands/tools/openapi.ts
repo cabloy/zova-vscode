2f3c79a254d2f05703d6fc44057af06fe8ea3fc9
zhennann <zhen.nann@icloud.com>
2024-12-27 23:32:52 +0800

    Update openapi.ts

--- src/commands/tools/openapi.ts
+++ src/commands/tools/openapi.ts
@@ -39,9 +39,6 @@ export async function toolsOpenapiGenerate(resource?: Uri) {
   // open
   const fileDest = commandPathInfo.moduleName
     ? path.join(commandPathInfo.moduleRoot, `src/service/_openapi_.ts`)
-    : path.join(
-        commandPathInfo.projectCurrent,
-        `src/suite/a-home/modules/home-api/src/service/_openapi_.ts`
-      );
+    : 'src/suite/a-home/modules/home-api/src/service/_openapi_.ts';
   showTextDocument(path.join(commandPathInfo.projectCurrent, fileDest));
 }
